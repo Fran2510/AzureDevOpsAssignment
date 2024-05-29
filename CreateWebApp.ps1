829b416c7084640403e4593410d1ffabc64c50c8
Fran2510 <60360465+Fran2510@users.noreply.github.com>
2024-05-29 22:12:49 +0200

    Add files via upload

---
+++ CreateWebApp.ps1
@@ -0,0 +1,23 @@
+﻿#script was run in Azure Cloud Shell
+
+$resourceGroupName = "RG-01"
+$appName = "WebAppSysKit4"
+$location = "West Europe"
+$appServicePlanName = "ASP-RG01-98bb"
+$pricingTier = "F1"
+
+#create web app
+$appServicePlan = Get-AzAppServicePlan -ResourceGroupName $resourceGroupName -Name $appServicePlanName -ErrorAction SilentlyContinue
+if (-not $appServicePlan) {
+    $appServicePlan = New-AzAppServicePlan -ResourceGroupName $resourceGroupName -Location $location -Name $appServicePlanName -Tier $pricingTier -WorkerSize 0
+}
+
+New-AzWebApp -ResourceGroupName $resourceGroupName -Name $appName -Location $location -AppServicePlan $appServicePlan.Name
+
+#apply tags
+$tags = @{
+    Project     = "AzureDevOpsAssignment"
+    Environment = "Staging"
+    Owner       = "Fran_M"
+}
+Set-AzResource -ResourceGroupName $resourceGroupName -ResourceName $appName -ResourceType "Microsoft.Web/sites" -Tag $tags -Force
